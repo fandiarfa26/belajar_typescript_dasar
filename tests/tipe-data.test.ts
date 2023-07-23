896ddc79d87c5e14109dada80dcb091bdd77f2d5
M. Fandi Arfabuma <arfabuma.987@gmail.com>
2023-07-23 11:38:02 +0700

    tipe data primitif

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,10 @@
+describe("Data type", () => {
+  it("should must declare", () => {
+    const name: string = "M. Fandi Arfabuma";
+    const money: number = 10000;
+    const isVip: boolean = false;
+    console.info(name);
+    console.info(money);
+    console.info(isVip);
+  });
+});
