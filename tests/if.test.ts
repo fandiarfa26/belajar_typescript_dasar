4040ea17d81953c6fe498151a78132334c4d3db3
M. Fandi Arfabuma <arfabuma.987@gmail.com>
2023-07-24 14:51:21 +0700

    switch statement

--- tests/if.test.ts
+++ tests/if.test.ts
@@ -10,9 +10,27 @@ describe("If Statement", () => {
       console.info("Try Again");
     }
   });
+
   it("should support ternary operator", () => {
     const value = 80;
     const say = value > 75 ? "Congrats" : "Oops";
     console.info(say);
+  });
+
+  it("should support switch statement", () => {
+    function sayHello(name: string): string {
+      switch (name) {
+        case "Fandi":
+          return "Hi Fandi";
+        case "Arfabuma":
+          return "Hi Arfabuma";
+        default:
+          return "Hello";
+      }
+    }
+
+    expect(sayHello("Fandi")).toBe("Hi Fandi");
+    expect(sayHello("Arfabuma")).toBe("Hi Arfabuma");
+    expect(sayHello("Eko")).toBe("Hello");
   });
 });
