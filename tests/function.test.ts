aed6e3263f158973d6776c769f2af92c2108699b
M. Fandi Arfabuma <arfabuma.987@gmail.com>
2023-07-24 14:23:52 +0700

    function parameter

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -12,4 +12,38 @@ describe("Function", () => {
 
     printHello("Fandi");
   });
+
+  it("should support default value", () => {
+    const sayHello = (name: string = "Guest"): string => {
+      return `Hello ${name}`;
+    };
+
+    expect(sayHello()).toBe("Hello Guest");
+    expect(sayHello("Fandi")).toBe("Hello Fandi");
+  });
+
+  it("should support rest parameter", () => {
+    const sum = (...values: number[]): number => {
+      let total = 0;
+      for (const value of values) {
+        total += value;
+      }
+      return total;
+    };
+
+    expect(sum(1, 2, 3, 4)).toBe(10);
+  });
+
+  it("should support optional parameter", () => {
+    const sayHello = (firstName: string, lastName?: string): string => {
+      if (lastName) {
+        return `Hello ${firstName} ${lastName}`;
+      } else {
+        return `Hello ${firstName}`;
+      }
+    };
+
+    expect(sayHello("Fandi")).toBe("Hello Fandi");
+    expect(sayHello("Fandi", "Arfabuma")).toBe("Hello Fandi Arfabuma");
+  });
 });
